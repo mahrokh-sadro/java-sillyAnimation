1ee49b8cf1516596386898b40ffcd836771d0091
AutoLogon <msadrolodabaee@myseneca.ca>
2021-09-02 17:34:33 -0400

    starting

---
+++ src/sillyFirstAnimation/Main.java
@@ -0,0 +1,11 @@
+package sillyFirstAnimation;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		MyFrame myFrame=new MyFrame();
+
+	}
+
+}
---
+++ src/sillyFirstAnimation/MyFrame.java
@@ -0,0 +1,12 @@
+package sillyFirstAnimation;
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame{
+
+	public MyFrame() {
+		setSize(840,840);
+	     setDefaultCloseOperation(exit_	
+		setVisible(true);
+	}
+}
