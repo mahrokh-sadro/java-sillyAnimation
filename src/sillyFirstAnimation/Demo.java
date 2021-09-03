1725ba3a3ff23d29a11bffc305a072590be5b508
AutoLogon <msadrolodabaee@myseneca.ca>
2021-09-02 20:03:16 -0400

    making the sun

--- src/sillyFirstAnimation/Demo.java
+++ src/sillyFirstAnimation/Demo.java
@@ -7,7 +7,12 @@ import javax.swing.*;
 
 public class Demo extends JPanel implements ActionListener{
 
-	Timer m_timer=new Timer(1000,this);
+	Timer m_timer=new Timer(1,this);
+	int m_xForSun;
+	int m_yForSun;
+	int m_xPosChange=1;
+	
+	
 	
 	public Demo() {
 		
@@ -19,13 +24,25 @@ public class Demo extends JPanel implements ActionListener{
 		super.paintComponent(g);
 		this.setBackground(Color.cyan);
 		
+		Graphics2D g2d=(Graphics2D) g;
+		
+		g2d.setColor(Color.yellow);
+		g2d.fillOval(m_xForSun, m_yForSun, 100, 100);
+		
+		
+		
+		
 	}
 	
 	
 	
 	@Override	
 	public void actionPerformed(ActionEvent e) {
-	
+		if(m_xForSun>800 || m_xForSun<0) m_xPosChange=-m_xPosChange;
+
+		m_xForSun+=m_xPosChange;
+		repaint();
+
 	}
 	
 	
